bfbe3db5c12a3a0c97e16fbcf1c0784ea5922105
Shivam <shivam.verma.shiva.99@gmail.com>
2024-09-08 22:04:34 +0530

    New changes Done and some Error is solved

---
+++ Practice.ts
@@ -0,0 +1,13 @@
+interface User {
+    name: string;
+    age: number;
+    password: string;
+}
+
+const userData: User = {
+    name: 'shivam',
+    age: 80,
+    password: 'shivam'
+};
+
+console.log(userData);
